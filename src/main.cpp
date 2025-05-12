b2d6a49bfa60485d87dc2b63edbfb44d9d09b280
gaetan <gaetan.coraboeuf@etud.univ-angers.fr>
2025-05-12 11:16:06 +0200

    lecture analogique

---
+++ src/main.cpp
@@ -0,0 +1,16 @@
+#include <Arduino.h>
+
+#define IR_PIN 14
+
+void setup() {
+  // put your setup code here, to run once:
+  Serial.begin(115200);
+  Serial.println("Capteur IR");
+}
+
+void loop() {
+  // put your main code here, to run repeatedly:
+  Serial.printf("Capteur IR: %d\n", analogRead(IR_PIN));
+  delay(1000);
+}
+
